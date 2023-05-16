c309e5d459f0704f1ae9984df464c90990d2b985
= <schulz.senol@gmail.com>
2023-05-16 17:33:45 +0200

    Added other tokens

--- src/lexer/c1.rs
+++ src/lexer/c1.rs
@@ -2,28 +2,62 @@ use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
 pub enum C1Token {
+    // Keywords
     #[token("bool")]
     KwBoolean,
+
     #[token("do")]
     KwDo,
+
     #[token("else")]
     KwElse,
+
     #[token("float")]
     KwFloat,
+
     #[token("for")]
     KwFor,
+
     #[token("if")]
     KwIf,
+
     #[token("int")]
     KwInt,
+
     #[token("printf")]
     KwPrintf,
+
     #[token("return")]
     KwReturn,
+
     #[token("void")]
     KwVoid,
+
     #[token("while")]
     KwWhile,
+
+
+
+    // Other Tokens
+    #[token(",")]
+    Comma,
+
+    #[token(";")]
+    Semicolon,
+
+    #[token("(")]
+    LParen,
+
+    #[token(")")]
+    RParen,
+
+    #[token("{")]
+    LBrace,
+
+    #[token("}")]
+    RBrace,
+
+
     #[error]
     Error,
 }
