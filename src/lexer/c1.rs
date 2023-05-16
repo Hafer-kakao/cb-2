c5e08f6141824165bca7da3da76cc6e1a360fd74
= <schulz.senol@gmail.com>
2023-05-16 17:15:34 +0200

    Added KwBoolean and KwDo

--- src/lexer/c1.rs
+++ src/lexer/c1.rs
@@ -2,10 +2,10 @@ use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
 pub enum C1Token {
-    // TODO: Define variants and their token/regex
-
-    // Logos requires one token variant to handle errors,
-    // it can be named anything you wish.
+    #[token("bool")]
+    KwBoolean,
+    #[token("do")]
+    KwDo,
     #[error]
     Error,
 }
